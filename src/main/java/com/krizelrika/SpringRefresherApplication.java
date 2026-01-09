9cda1004d0f45687761928fcb4185e5c3fd9fe80
Krizel Rika <krizelcodes@gmail.com>
2026-01-09 15:02:21 +0800

    Hello API

--- src/main/java/com/krizelrika/SpringRefresherApplication.java
+++ src/main/java/com/krizelrika/SpringRefresherApplication.java
@@ -2,12 +2,21 @@ package com.krizelrika;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringRefresherApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
+
         SpringApplication.run(SpringRefresherApplication.class, args);
+    }
+
+    @GetMapping
+    public String helloWorld() {
+        return "Hello Spring";
     }
 
 }
