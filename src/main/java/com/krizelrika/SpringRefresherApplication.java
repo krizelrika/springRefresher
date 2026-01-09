4f152113165192a28ab7baae729e42747738aa49
Krizel Rika <krizelcodes@gmail.com>
2026-01-09 14:38:37 +0800

    Initial commit

---
+++ src/main/java/com/krizelrika/SpringRefresherApplication.java
@@ -0,0 +1,13 @@
+package com.krizelrika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRefresherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRefresherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krizelrika/SpringRefresherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krizelrika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRefresherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
