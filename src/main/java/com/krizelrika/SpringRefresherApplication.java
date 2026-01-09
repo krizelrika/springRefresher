e75763f0a226541218c5a016feed8ff209cb93a0
Krizel Rika <krizelcodes@gmail.com>
2026-01-09 14:39:01 +0800

    initial files

--- src/main/java/com/krizelrika/SpringRefresherApplication.java
+++ src/main/java/com/krizelrika/SpringRefresherApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringRefresherApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringRefresherApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRefresherApplication.class, args);
+    }
 
 }
